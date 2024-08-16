d2627ed74d657fac45b54ab72d43bf4d755f41d2
Eliott <eliott.jacopin@laposte.net>
2024-08-16 18:37:43 +0900

    [Update] Profile overwrite of uds[ac] files in C++
    Major:
    - Replaced the test function to see if OpenUSD and the Profiler had been correctly integrated to the project by the generation of the usda and usdc files.
    - We are using our profiler to measure the time for the same functions we were following for the code written in Python
    - We are using a single thread version just to make sure it works and we can start measuring.
    
    Minor:
    - Added a compile-time copy of the reference usda and usdc files to be in a folder called `Assets` at a known path for the exe to get them.

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,59 +1,84 @@
-#include <iostream>
-
-// for the minimal example involving the creation of a USD stage in TestFunction_StageCreation
-#include "pxr/usd/usd/stage.h" 
-
-// For TestFunction_PixarTutorial_HelloWorld
-#include "pxr/usd/usdGeom/xform.h" 
-#include "pxr/usd/usdGeom/sphere.h"
-#include "pxr/usd/sdf/path.h"
-
+#include <pxr/usd/usd/stage.h>
+#include <pxr/usd/usdGeom/xform.h>
+#include <pxr/usd/usdGeom/xformOp.h>
 #include "Profile/Profiler.hpp"
 
-/*!
-@brief Function reproducing the first item of the Pixar USD tutorial.
-@see https://openusd.org/release/tut_helloworld.html
-	 https://openusd.org/release/tut_helloworld_redux.html
-*/
-void TestFunction_PixarTutorial_HelloWorld()
+#include <iostream>
+#include <string>
+
+pxr::GfVec3f RandomVec3(float scaleX = 1.0, float scaleY = 1.0, float scaleZ = 1.0)
+{
+	return pxr::GfVec3f( scaleX, scaleY, scaleZ );
+}
+
+void AddRandomPlaceReferencesInStage(pxr::UsdStageRefPtr stage, const std::string& path, const std::string& baseName, const std::string& refUSDFilePath, int refCount = 10)
 {
 	PROFILE_FUNCTION_TIME(0);
+	for (int i = 0; i < refCount; ++i)
+	{
+		std::string primPath = path + "/" + baseName + "_" + std::to_string(i);
+		pxr::UsdPrim refCube = stage->OverridePrim(pxr::SdfPath(primPath));
+		refCube.GetReferences().AddReference(refUSDFilePath);
+		pxr::UsdGeomXformable refXform(refCube);
+		refXform.AddTranslateOp(pxr::UsdGeomXformOp::PrecisionFloat).Set(RandomVec3(10, 10, 10));
+		refXform.AddRotateXYZOp(pxr::UsdGeomXformOp::PrecisionFloat).Set(RandomVec3(180, 180, 180));
+		refXform.AddScaleOp(pxr::UsdGeomXformOp::PrecisionFloat).Set(RandomVec3(2, 2, 2));
+	}
+}
 
-	std::cout << "** TestFunction_PixarTutorial_HelloWorld **" << std::endl;
+void WriteUSDStage(int nbRefs, int nbBatch, const std::string& usdExtension, int nbRepeats = 100)
+{
+	pxr::UsdStageRefPtr stage;
+	for (int rep = 0; rep < nbRepeats; ++rep)
+	{
+		for (int fileNumber = 0; fileNumber < nbBatch; ++fileNumber)
+		{
+			{
+				PROFILE_BLOCK_TIME("Create New Stage", 0);
+				stage = pxr::UsdStage::CreateNew("./Temp/Cubes_" + std::to_string(rep) + "_" + std::to_string(fileNumber) + "." + usdExtension);
+			}
 
-	/* Python code from the tutorial
+			{
+				PROFILE_BLOCK_TIME("Define World", 0);
+				pxr::UsdGeomXform::Define(stage, pxr::SdfPath("/World"));
+			}
 
-		from pxr import Usd, UsdGeom
-		stage = Usd.Stage.CreateNew('HelloWorld.usda')
-		xformPrim = UsdGeom.Xform.Define(stage, '/hello')
-		spherePrim = UsdGeom.Sphere.Define(stage, '/hello/world')
-		stage.GetRootLayer().Save()
-	*/
+			AddRandomPlaceReferencesInStage(stage, "/World", "Cube", "../../Assets/SimpleTransform." + usdExtension, nbRefs / nbBatch);
 
-	//Write the C++ equivalent of the above python code
-	pxr::UsdStageRefPtr stage = pxr::UsdStage::CreateNew("HelloWorld.usda");
-	pxr::UsdGeomXform xformPrim = pxr::UsdGeomXform::Define(stage, pxr::SdfPath("/hello"));
-	pxr::UsdGeomSphere spherePrim = pxr::UsdGeomSphere::Define(stage, pxr::SdfPath("/hello/world"));
-
-	std::string fileResult;
-	stage->GetRootLayer()->ExportToString(&fileResult);
-	std::cout << "Content of file HelloWorld.usda:\n" << fileResult << std::endl;
-
-	stage->GetRootLayer()->Save(); // Save the stage at the same location as the application.
+			{
+				PROFILE_BLOCK_TIME("Save Stage", 0);
+				stage->GetRootLayer()->Save();
+			}
+		}
+	}
 }
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-
 	Profile::Profiler* profiler = (Profile::Profiler*)calloc(1, sizeof(Profile::Profiler));
 	profiler->SetProfilerName("OpenUSD-Writing-Benchmark");
-
+	
 	Profile::SetProfiler(profiler);
 	profiler->SetTrackName(0, "Main");
-	profiler->Initialize();
+	
+	int nbRepeats = 10;
+	std::vector<int> nbRefs = { 10 };//, 100, 1000};
+	std::vector<int> nbBatch = { 1 };// , 2, 5, 10};
+	std::vector<std::string> usdExtension = { "usda", "usdc" };
 
-	TestFunction_PixarTutorial_HelloWorld();
+	profiler->Initialize();
+	for (int refs : nbRefs)
+	{
+		for (int batch : nbBatch)
+		{
+			for (const auto& ext : usdExtension)
+			{
+				std::cout << "refs: " << refs << ", batch: " << batch << ", ext: " << ext << std::endl;
+				WriteUSDStage(refs, batch, ext, nbRepeats);
+				std::cout << std::endl;
+			}
+		}
+	}
 
 	profiler->End();
 	profiler->Report();
