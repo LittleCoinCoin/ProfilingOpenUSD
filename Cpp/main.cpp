6e1107536b54bd3f0b0d6276aaa195fa66f8bf93
Eliott <eliott.jacopin@laposte.net>
2024-08-23 11:49:03 +0900

    [WIP] Use repetition profiler

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -26,10 +26,19 @@ void AddRandomPlaceReferencesInStage(pxr::UsdStageRefPtr stage, const std::strin
 	}
 }
 
-void WriteUSDStage(int nbRefs, int nbBatch, const std::string& usdExtension, int nbRepeats = 100)
+
+struct WriteUSDStage_RepetitionTest : public Profile::RepetitionTest
 {
 	pxr::UsdStageRefPtr stage;
-	for (int rep = 0; rep < nbRepeats; ++rep)
+
+	int nbRefs = 0;
+	int nbBatch = 1;
+	int rep = 0;
+	std::string usdExtension = "usda";
+
+	WriteUSDStage_RepetitionTest() = default;
+
+	inline void operator()() override
 	{
 		for (int fileNumber = 0; fileNumber < nbBatch; ++fileNumber)
 		{
@@ -51,7 +60,60 @@ void WriteUSDStage(int nbRefs, int nbBatch, const std::string& usdExtension, int
 			}
 		}
 	}
-}
+
+	/*void SetParameters(int _nbRefs, int _nbBatch, int _rep, const std::string& _usdExtension)
+	{
+		nbRefs = _nbRefs;
+		nbBatch = _nbBatch;
+		rep = _rep;
+		usdExtension = _usdExtension;
+	}*/
+};
+
+//void WriteUSDStage_FixedRepetitionTesting()
+//{
+//	Profile::u64* arr = (Profile::u64*)malloc(sizeof(Profile::u64) * 8192);
+//	RepetitionTest_TestFunction_ProfileFunction repetitionTest(arr, 8192);
+//
+//	Profile::u16 repetitionCount = 1000;
+//	Profile::RepetitionProfiler* repetitionProfiler = (Profile::RepetitionProfiler*)calloc(1, sizeof(Profile::RepetitionProfiler));
+//	Profile::ProfilerResults* results = (Profile::ProfilerResults*)calloc(repetitionCount, sizeof(Profile::ProfilerResults));
+//
+//	repetitionProfiler->SetRepetitionResults(results);
+//	repetitionProfiler->FixedCountRepetitionTesting(repetitionCount, repetitionTest);
+//	repetitionProfiler->Report(repetitionCount);
+//
+//	free(results);
+//	free(repetitionProfiler);
+//	free(arr);
+//}
+
+//void WriteUSDStage(int nbRefs, int nbBatch, const std::string& usdExtension)
+//{
+//	pxr::UsdStageRefPtr stage;
+//	for (int rep = 0; rep < nbRepeats; ++rep)
+//	{
+//		for (int fileNumber = 0; fileNumber < nbBatch; ++fileNumber)
+//		{
+//			{
+//				PROFILE_BLOCK_TIME("Create New Stage", 0);
+//				stage = pxr::UsdStage::CreateNew("./Temp/Cubes_" + std::to_string(rep) + "_" + std::to_string(fileNumber) + "." + usdExtension);
+//			}
+//
+//			{
+//				PROFILE_BLOCK_TIME("Define World", 0);
+//				pxr::UsdGeomXform::Define(stage, pxr::SdfPath("/World"));
+//			}
+//
+//			AddRandomPlaceReferencesInStage(stage, "/World", "Cube", "../../Assets/SimpleTransform." + usdExtension, nbRefs / nbBatch);
+//
+//			{
+//				PROFILE_BLOCK_TIME("Save Stage", 0);
+//				stage->GetRootLayer()->Save();
+//			}
+//		}
+//	}
+//}
 
 int main()
 {
@@ -60,11 +122,21 @@ int main()
 	
 	Profile::SetProfiler(profiler);
 	profiler->SetTrackName(0, "Main");
+
+	WriteUSDStage_RepetitionTest writeUSDStage_Rep;
+
+	
+
 	
 	int nbRepeats = 10;
 	std::vector<int> nbRefs = { 10 };//, 100, 1000};
 	std::vector<int> nbBatch = { 1 };// , 2, 5, 10};
 	std::vector<std::string> usdExtension = { "usda", "usdc" };
+
+	Profile::RepetitionProfiler* repetitionProfiler = (Profile::RepetitionProfiler*)calloc(1, sizeof(Profile::RepetitionProfiler));
+	Profile::ProfilerResults* results = (Profile::ProfilerResults*)calloc(nbRepeats, sizeof(Profile::ProfilerResults));
+
+	repetitionProfiler->SetRepetitionResults(results);
 
 	profiler->Initialize();
 	for (int refs : nbRefs)
@@ -74,7 +146,11 @@ int main()
 			for (const auto& ext : usdExtension)
 			{
 				std::cout << "refs: " << refs << ", batch: " << batch << ", ext: " << ext << std::endl;
-				WriteUSDStage(refs, batch, ext, nbRepeats);
+
+				//writeUSDStage_Rep->SetParameters(refs, batch, nbRepeats, ext);
+				repetitionProfiler->FixedCountRepetitionTesting(nbRepeats, writeUSDStage_Rep, false, true);
+
+				repetitionProfiler->Report(nbRepeats);
 				std::cout << std::endl;
 			}
 		}
@@ -83,6 +159,8 @@ int main()
 	profiler->End();
 	profiler->Report();
 
+	free(results);
+	free(repetitionProfiler);
 	free(profiler);
 
 	return 0;
